77085e9134742f01d7b3e9425b2f04f92637ae8f
Sorah Gleich <sorahgleich@gmail.com>
2023-08-22 15:28:22 -0400

    create website base

---
+++ style.js
@@ -0,0 +1,20 @@
+const image1 = document.getElementById('gitbash')
+const image2 = document.getElementById('task-chart')
+const image3 = document.getElementById('wine-festival')
+const button = document.getElementbyId('button1')
+
+button.innerHTML = '<p>jsdhf</p>'
+image1.style.display = 'block'
+
+
+/*function seeMore() {
+    button.innerHTML = "See Less";
+    image1.hidden = false;
+    image2.hidden = false;
+    image3.hidden = false;
+
+}
+    
+Button.addEventListener("mouseover", seeMore);*/
+
+
