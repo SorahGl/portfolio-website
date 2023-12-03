31990b5b63d48c83d025cefb864c0f5aded6a4ed
Sorah Gleich <sorahgleich@gmail.com>
2023-12-02 22:27:02 -0500

    some final interactivity

--- style.js
+++ style.js
@@ -3,24 +3,42 @@ let image1 = document.getElementById('gitbash');
 let image2 = document.getElementById('wordle');
 let image3 = document.getElementById('wine-festival');
 let button1 = document.getElementById('button1');
-//let button2 = document.getElementById('button2');
-
-
+let project = document.getElementsByClassName('project-div');
+let info = document.getElementsByClassName('info');
 
 function seeMore() {
     button1.innerHTML === 'See Less'? button1.innerHTML = 'See More Projects' : button1.innerHTML = 'See Less' ;
-    //button1.setAttribute('id', 'button2');
-    (image1.hidden === false)? image1.hidden = true : image1.hidden = false;
+    image1.hidden === false? image1.hidden = true : image1.hidden = false;
     image2.hidden === false? image2.hidden = true : image2.hidden = false;
     image3.hidden === false? image3.hidden = true : image3.hidden = false;
-    
-    console.log('hi');
 }
-
 
 function buttonEvents(button1) {
   button1.addEventListener('click', seeMore);
-}    
+} 
+
+const projectEvents = () => {
+  for (let i = 0; i < project.length; i++) {
+        project[i].addEventListener('mouseenter', function showLinks() {
+          //setTimeout(() => {
+            
+            info[i].style.display = 'flex';
+            info[i].style.transition = 'all 2s ease-in-out';
+          //}, 180);
+         } )
+    }     
+  }
+        
+
+const projectNonEvents = () => {
+  for (let i = 0; i < project.length; i++) {
+        project[i].addEventListener('mouseleave', function dontShowLinks() {
+        info[i].style.display = 'none';
+        });
+    }
+}
 
 
+projectEvents();
+projectNonEvents();
 buttonEvents(button1);
